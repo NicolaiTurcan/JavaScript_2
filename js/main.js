f9e29cf26f3e776b6b3bb3a59f2f81fb08a9b4e0
NicolaiTurcan <Nicki@list.ru>
2021-08-02 13:47:12 +0200

    Homework - i done only the first task

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 'use strict';
 
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+let getRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status !== 200) {
+                    reject("failure");
+                } else {
+                    resolve(xhr.responseText);
+                }
+            }
+        }
+        xhr.send();
+    });
+}
+
+
 class GoodsItem {
-    constructor(title, price, image = "https://picsum.photos/seed/9/200", altImage = "Pictures") {
-        this.title = title;
+    constructor(product_name, price, image = "https://picsum.photos/seed/9/200", altImage = "Pictures") {
+        this.product_name = product_name;
         this.price = price;
         this.image = image;
         this.altImage = altImage;
     }
-    rendredProduct() {
+    renderedProduct() {
         return `<div class="product-item">
                     <img src="${this.image}" alt="${this.altImage}">
-                    <h3>${this.title}</h3>
+                    <h3>${this.product_name}</h3>
                     <p>Price: ${this.price}</p>
                     <button class="by-btn">Добавить</button>
                 </div>`;
@@ -20,37 +40,34 @@ class GoodsItem {
 class GoodsList {
     constructor() {
         this.goods = [];
+        this._getProducts();
     }
-    fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 350 },
-            { title: 'Shoes', price: 250 },
-        ];
+    _getProducts() {
+        return getRequest(`${API}/catalogData.json`)
+            .then(response => {
+                this.goods = JSON.parse(response);
+                this.render();
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.rendredProduct();
+            const goodItem = new GoodsItem(good.product_name, good.price);
+            listHtml += goodItem.renderedProduct();
         });
         document.querySelector('.products').innerHTML = listHtml;
-    }
-    // Lesson 2.2(Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.)
-    getTotalPrice() {
-        let totalPrice = 0;
-        this.goods.forEach(good => {
-            totalPrice += good.price;
-        });
-        console.log(totalPrice);
     }
 }
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.getTotalPrice();
+
+
+
+
+
 
 class Cart {
     constructor() {
